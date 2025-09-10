a2405c1ebc9424888c0548204837e2a9dce784cc
FaizaIftikhar <iamfaiza8o9@gmail.com>
2025-09-10 14:35:06 +0100

    Using use state to create four pieces of state email,password,message and showPassword and then export the component so that it can be used on App.jsx

---
+++ login-form/src/LoginForm.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+function LoginForm()
+{
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [message,setMessage]=useState("")
+ const [showPassword, setShowPassword] = useState(false);
+
+
+    
+    return(
+        <div> 
+        
+        </div>
+    );
+
+}
+export default  LoginForm
